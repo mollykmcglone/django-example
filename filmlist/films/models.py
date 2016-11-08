2a83069d66860fa4ab4b2971ed6264ce44754e50
Liz Tom <liz.tom@gmail.com>
2016-11-07 19:23:26 -0800

    example

---
+++ filmlist/films/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Film(models.Model):
+    title = models.CharField(max_length=100, blank=True, default='')
+    year_prod = models.IntegerField()
+    genre = models.CharField(max_length=100, blank=True, default='')
+
+    class Meta:
+        ordering = ('title',)
