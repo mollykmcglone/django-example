9f53d3d0f23e3e2f8072a0bcfcd6f1f2275745ce
Molly McGlone <mollykmcglone@gmail.com>
2016-11-28 20:58:42 -0800

    update views.py to use generic framework requests and filter by year

--- filmlist/films/views.py
+++ filmlist/films/views.py
@@ -1,141 +1,46 @@
 from films.models import *
 from films.serializers import *
 from django.http import Http404
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+import pdb
 
+class FilmList(generics.ListCreateAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
 
-class FilmList(APIView):
-    """
-    List all films, or create a new film.
-    """
-    def get(self, request, format=None):
-        films = Film.objects.all()
+    def get(self, request, *args, **kwargs):
+        title = request.GET.get('title', '')
+        year_prod = request.GET.get('year_prod', '')
+        if year_prod == '':
+            films = Film.objects.all()
+        else:
+            films = Film.objects.filter(year_prod=year_prod)
+        if title == '':
+            films = Film.objects.all()
+        else:
+            films = Film.objects.filter(title__istartswith=title)
         serialized_films = FilmSerializer(films, many=True)
         return Response(serialized_films.data)
 
-    def post(self, request, format=None):
-        film = FilmSerializer(data=request.data)
-        if film.is_valid():
-            film.save()
-            return Response(film.data, status=status.HTTP_201_CREATED)
-        return Response(film.errors, status=status.HTTP_400_BAD_REQUEST)
+class FilmDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
 
+class TheaterList(generics.ListCreateAPIView):
+    queryset = Theater.objects.all()
+    serializer_class = TheaterSerializer
 
-class FilmDetail(APIView):
-    """
-    Retrieve, update or delete a film instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Film.objects.get(pk=pk)
-        except Film.DoesNotExist:
-            raise Http404
+class TheaterDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Theater.objects.all()
+    serializer_class = TheaterSerializer
 
-    def get(self, request, pk, format=None):
-        film = self.get_object(pk)
-        serialized_film = FilmSerializer(film)
-        return Response(serialized_film.data)
+class GenreList(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
 
-    def put(self, request, pk, format=None):
-        film = self.get_object(pk)
-        serialized_film = FilmSerializer(film, data=request.data)
-        if serialized_film.is_valid():
-            serialized_film.save()
-            return Response(serialized_film.data)
-        return Response(serialized_film.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        film = self.get_object(pk)
-        film.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-class TheaterList(APIView):
-    """
-    List all theaters, or create a new theater.
-    """
-    def get(self, request, format=None):
-        theaters = Theater.objects.all()
-        serialized_theaters = TheaterSerializer(theaters, many=True)
-        return Response(serialized_theaters.data)
-
-    def post(self, request, format=None):
-        theater = TheaterSerializer(data=request.data)
-        if theater.is_valid():
-            theater.save()
-            return Response(theater.data, status=status.HTTP_201_CREATED)
-        return Response(theater.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class TheaterDetail(APIView):
-    """
-    Retrieve, update or delete a theater instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Theater.objects.get(pk=pk)
-        except Theater.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        theater = self.get_object(pk)
-        return Response(serialized_theater.data)
-
-    def put(self, request, pk, format=None):
-        theater = self.get_object(pk)
-        serialized_theater = TheaterSerializer(theater, data=request.data)
-        if serialized_theater.is_valid():
-            serialized_theater.save()
-            return Response(serialized_theater.data)
-        return Response(serialized_theater.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        theater= self.get_object(pk)
-        theater.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-class GenreList(APIView):
-    """
-    List all snippets, or create a new film.
-    """
-    def get(self, request, format=None):
-        genres = Genre.objects.all()
-        serialized_genres = GenreSerializer(genres, many=True)
-        return Response(serialized_genres.data)
-
-    def post(self, request, format=None):
-        genre = GenreSerializer(data=request.data)
-        if genre.is_valid():
-            genre.save()
-            return Response(genre.data, status=status.HTTP_201_CREATED)
-        return Response(genre.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class GenreDetail(APIView):
-    """
-    Retrieve, update or delete a film instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Genre.objects.get(pk=pk)
-        except Genre.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        genre = self.get_object(pk)
-        serialized_genre = GenreSerializer(genre)
-        return Response(serialized_genre.data)
-
-    def put(self, request, pk, format=None):
-        genre = self.get_object(pk)
-        serialized_genre = GenreSerializer(film, data=request.data)
-        if serialized_genre.is_valid():
-            serialized_film.save()
-            return Response(serialized_film.data)
-        return Response(serialized_film.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        genre = self.get_object(pk)
-        genre.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class GenreDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
